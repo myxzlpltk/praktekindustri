b8ea5a9650db34f993a705cfe51e8b018b7b9d99
Saddam Azy <myxzlpltk@gmail.com>
2021-01-25 06:08:54 +0700

    force ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Carbon\Carbon;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,16 +28,20 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot(){
-    	/* Load Setting */
-			if (Schema::hasTable('settings')) {
-				foreach (Setting::all() as $setting) {
-					Config::set('settings.'.$setting->key, $setting->value);
-				}
-			}
+		if($this->app->environment('production')) {
+			URL::forceScheme('https');
+		}
 
-			/* Locale id_ID */
-			config(['app.locale' => 'id']);
-			Carbon::setLocale('id');
-			Paginator::useBootstrap();
+    	/* Load Setting */
+		if (Schema::hasTable('settings')) {
+			foreach (Setting::all() as $setting) {
+				Config::set('settings.'.$setting->key, $setting->value);
+			}
+		}
+
+		/* Locale id_ID */
+		config(['app.locale' => 'id']);
+		Carbon::setLocale('id');
+		Paginator::useBootstrap();
     }
 }
