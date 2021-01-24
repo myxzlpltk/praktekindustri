32ce19e5598c0960cdec72a0f2f2e82b479466b8
Catyousha <jakaasha@gmail.com>
2021-01-24 17:40:21 +0700

    fix division by zero dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -64,37 +64,37 @@
 
 				<h4 class="small font-weight-bold">Jumlah Akun Mahasiswa Terdaftar<span class="float-right">{{$statistik->total_mhs}}</span></h4>
 				<div class="progress mb-4">
-					<div class="progress-bar bg-primary" role="progressbar" style="width: {{($statistik->total_mhs / 427) * 100}}%" aria-valuenow="{{$statistik->total_mhs}}" aria-valuemin="0" aria-valuemax="427"></div>
+					<div class="progress-bar bg-primary" role="progressbar" style="width: {{($statistik->total_mhs == 0)?0:($statistik->total_mhs / 427) * 100}}%" aria-valuenow="{{$statistik->total_mhs}}" aria-valuemin="0" aria-valuemax="427"></div>
 				</div>
 
 				<h4 class="small font-weight-bold">Total Proposal Disubmit <span class="float-right">{{$statistik->total_proposal}}</span></h4>
 				<div class="progress mb-4">
-					<div class="progress-bar bg-info" role="progressbar" style="width: {{($statistik->total_proposal / $statistik->total_mhs) * 100}}%" aria-valuenow="{{$statistik->total_proposal}}" aria-valuemin="0" aria-valuemax="{{$statistik->total_mhs}}"></div>
+					<div class="progress-bar bg-info" role="progressbar" style="width: {{($statistik->total_proposal == 0)?0:($statistik->total_proposal / $statistik->total_mhs) * 100}}%" aria-valuenow="{{$statistik->total_proposal}}" aria-valuemin="0" aria-valuemax="{{$statistik->total_mhs}}"></div>
 				</div>
 
 				<h4 class="small font-weight-bold">Proposal Menunggu Pengesahan Koordinator <span class="float-right">{{$statistik->pr_wait_koor}}</span></h4>
 				<div class="progress mb-4">
-					<div class="progress-bar bg-warning" role="progressbar" style="width: {{($statistik->pr_wait_koor / $statistik->total_proposal) * 100}}%" aria-valuenow="{{$statistik->pr_wait_koor}}" aria-valuemin="0" aria-valuemax="{{$statistik->total_proposal}}"></div>
+					<div class="progress-bar bg-warning" role="progressbar" style="width: {{($statistik->total_proposal == 0)?0:($statistik->pr_wait_koor / $statistik->total_proposal) * 100}}%" aria-valuenow="{{$statistik->pr_wait_koor}}" aria-valuemin="0" aria-valuemax="{{$statistik->total_proposal}}"></div>
 				</div>
 
 				<h4 class="small font-weight-bold">Proposal Menunggu Pengesahan Ketua Jurusan <span class="float-right">{{$statistik->pr_wait_kajur}}</span></h4>
 				<div class="progress mb-4">
-					<div class="progress-bar bg-warning" role="progressbar" style="width: {{($statistik->pr_wait_kajur / $statistik->total_proposal) * 100}}%" aria-valuenow="{{$statistik->pr_wait_kajur}}" aria-valuemin="0" aria-valuemax="{{$statistik->total_proposal}}"></div>
+					<div class="progress-bar bg-warning" role="progressbar" style="width: {{($statistik->total_proposal == 0)?0:($statistik->pr_wait_kajur / $statistik->total_proposal) * 100}}%" aria-valuenow="{{$statistik->pr_wait_kajur}}" aria-valuemin="0" aria-valuemax="{{$statistik->total_proposal}}"></div>
 				</div>
 
 				<h4 class="small font-weight-bold">Proposal Ditolak oleh Koordinator <span class="float-right">{{$statistik->pr_tolak_koor}}</span></h4>
 				<div class="progress mb-4">
-					<div class="progress-bar bg-danger" role="progressbar" style="width: {{($statistik->pr_tolak_koor / $statistik->total_proposal) * 100}}%" aria-valuenow="{{$statistik->pr_tolak_koor}}" aria-valuemin="0" aria-valuemax="{{$statistik->total_proposal}}"></div>
+					<div class="progress-bar bg-danger" role="progressbar" style="width: {{($statistik->total_proposal == 0)?0:($statistik->pr_tolak_koor / $statistik->total_proposal) * 100}}%" aria-valuenow="{{$statistik->pr_tolak_koor}}" aria-valuemin="0" aria-valuemax="{{$statistik->total_proposal}}"></div>
 				</div>
 
 				<h4 class="small font-weight-bold">Proposal Ditolak oleh Ketua Jurusan <span class="float-right">{{$statistik->pr_tolak_kajur}}</span></h4>
 				<div class="progress mb-4">
-					<div class="progress-bar bg-danger" role="progressbar" style="width: {{($statistik->pr_tolak_kajur / $statistik->total_proposal) * 100}}%" aria-valuenow="{{$statistik->pr_tolak_kajur}}" aria-valuemin="0" aria-valuemax="{{$statistik->total_proposal}}"></div>
+					<div class="progress-bar bg-danger" role="progressbar" style="width: {{($statistik->total_proposal == 0)?0:($statistik->pr_tolak_kajur / $statistik->total_proposal) * 100}}%" aria-valuenow="{{$statistik->pr_tolak_kajur}}" aria-valuemin="0" aria-valuemax="{{$statistik->total_proposal}}"></div>
 				</div>
 
 				<h4 class="small font-weight-bold">Proposal Disahkan <span class="float-right">{{$statistik->pr_acc}}</span></h4>
 				<div class="progress mb-4">
-					<div class="progress-bar bg-success" role="progressbar" style="width: {{($statistik->pr_acc / $statistik->total_proposal) * 100}}%" aria-valuenow="{{$statistik->pr_acc}}" aria-valuemin="0" aria-valuemax="{{$statistik->total_proposal}}"></div>
+					<div class="progress-bar bg-success" role="progressbar" style="width: {{($statistik->total_proposal == 0)?0:($statistik->pr_acc / $statistik->total_proposal) * 100}}%" aria-valuenow="{{$statistik->pr_acc}}" aria-valuemin="0" aria-valuemax="{{$statistik->total_proposal}}"></div>
 				</div>
 
 				<p class="text-danger">Note: Jumlah proposal tidak sama dengan jumlah mahasiswa karena proposal yang ditolak juga diikutsertakan dalam data</p>
