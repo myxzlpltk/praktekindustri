a0ce7426c5341d7e943a7dc18b51f541045d14de
Catyousha <jakaasha@gmail.com>
2021-01-26 15:08:34 +0700

    prodi koor di seed kebalik

--- database/seeders/ProdiSeeder.php
+++ database/seeders/ProdiSeeder.php
@@ -17,32 +17,32 @@ class ProdiSeeder extends Seeder
     	$coordinators = Coordinator::all();
 
 		Prodi::create([
-			'coordinator_id' => $coordinators->get(0)->id,
+			'coordinator_id' => $coordinators->get(1)->id,
 			'name' => 'S1 Pendidikan Teknik Informatika',
 			'code' => 'S1PTI',
 		]);
 		Prodi::create([
-			'coordinator_id' => $coordinators->get(1)->id,
+			'coordinator_id' => $coordinators->get(0)->id,
 			'name' => 'S1 Pendidikan Teknik Elektro',
 			'code' => 'S1PTE',
 		]);
 		Prodi::create([
-			'coordinator_id' => $coordinators->get(0)->id,
+			'coordinator_id' => $coordinators->get(1)->id,
 			'name' => 'S1 Teknik Informatika',
 			'code' => 'S1TI',
 		]);
 		Prodi::create([
-			'coordinator_id' => $coordinators->get(1)->id,
+			'coordinator_id' => $coordinators->get(0)->id,
 			'name' => 'S1 Teknik Elektro',
 			'code' => 'S1TE',
 		]);
 		Prodi::create([
-			'coordinator_id' => $coordinators->get(1)->id,
+			'coordinator_id' => $coordinators->get(0)->id,
 			'name' => 'D3 Teknik Elektro',
 			'code' => 'D3ELKO',
 		]);
 		Prodi::create([
-			'coordinator_id' => $coordinators->get(0)->id,
+			'coordinator_id' => $coordinators->get(1)->id,
 			'name' => 'D3 Teknik Elektronika',
 			'code' => 'D3ELKA',
 		]);
